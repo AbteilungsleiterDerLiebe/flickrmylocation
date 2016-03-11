dca6d4305d320b9350dfd8ad25802eee471d9566
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-03-11 22:00:55 +0100

    pushed gitignore

--- app/src/main/java/de/hs_bremen/nhinte/flickrmylocation/MainActivity.java
+++ app/src/main/java/de/hs_bremen/nhinte/flickrmylocation/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     public void createUrlTextfield() {
 
         FlickrApiQueryManager flickrApiQueryManager = new FlickrApiQueryManager();
